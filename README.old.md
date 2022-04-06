18149849e6fd8eb55ce0ac9ae80b8e37e47b0d5c
taibui <tai.c.bui@gmail.com>
2022-04-06 10:44:52 +0700

    Scaffolding the project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# shopdunk-reactjs-ver1
+1st Reactjs Version of Shopdunk.com
---
+++ src/App.js
@@ -0,0 +1,50 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>iPhones</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>iPad</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Mac</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Watch</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>AirPods</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
