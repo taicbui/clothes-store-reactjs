1411f59c5f7f85d2823d7687475aebda1cb8365a
taibui <tai.c.bui@gmail.com>
2022-05-16 15:40:44 +0700

    commit before delete

--- src/store/categories/category.types.js
+++
@@ -1,7 +0,0 @@
-// 1st step: Types of category actions to be imported by 'category.action.js'
-
-export const CATEGORIES_ACTION_TYPES = {
-    FETCH_CATEGORIES_START: 'category/FETCH_CATEGORIES_START',
-    FETCH_CATEGORIES_SUCCESS: 'category/FETCH_CATEGORIES_SUCCESS',
-    FETCH_CATEGORIES_FAILED: 'category/FETCH_CATEGORIES_FAILED',
-  };
